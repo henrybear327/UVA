c4a89835b63c3428049d570fb9c7373993c5b2e1
Chun-Hung Tseng <henrybear327@gmail.com>
2015-08-27 20:04:27 +0800

    Add cin scanf testing code

---
+++ IO Test/C++_style.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int a, b;
+    for(int i = 0; i < 99999999; i++) {
+	cin >> a >> b;
+	//printf("%d\n", a - b);
+	cout << (a - b) << '\n';
+    }
+
+    return 0;
+}	
+    
---
+++ IO Test/generator.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main()
+{
+    for(int i = 0; i < 99999999; i++)
+	printf("%d %d\n", i, i);
+
+    return 0;
+}
