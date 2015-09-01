57788604b81f7faaf9eacd6ff4291678113f87dc
Chun-Hung Tseng <henrybear327@gmail.com>
2015-09-01 23:41:47 +0800

    Add 1 problem

---
+++ Data Structures and Libraries/Data Structures with Our-Own Libraries/Graph Data Structures Problems/11991 - Easy Problem from Rujia Liu?/main.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+vector<int> position[1000001];
+
+int main()
+{
+    int total, query;
+    while (scanf("%d %d", &total, &query) == 2) {
+        for (int i = 0; i < 1000001; i++)
+            position[i].clear();
+
+        for (int i = 1; i <= total; i++) {
+            int tmp;
+            scanf("%d", &tmp);
+
+            position[tmp].push_back(i);
+        }
+
+        while (query--) {
+            int occurence, number;
+            scanf("%d %d", &occurence, &number);
+
+            if ((int)position[number].size() < occurence) {
+                printf("0\n");
+                continue;
+            }
+
+            // UVa can't compile auto @@
+            // auto i = position[number].begin() + occurence - 1;
+            vector<int>::iterator i = position[number].begin() + occurence - 1;
+            printf("%d\n", *i);
+        }
+    }
+
+    return 0;
+}
