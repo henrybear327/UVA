dfaaf865a7935fb7159c9822865025624a46b146
Chun-Hung Tseng <henrybear327@gmail.com>
2015-09-23 21:22:31 +0800

    Add 488

---
+++ Random stuff/488 triangle waves/main.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+
+int main()
+{
+    int cases;
+    scanf("%d", &cases);
+
+    while (cases--) {
+        getchar();
+        int amp, freq;
+        scanf("%d %d", &amp, &freq);
+
+        if (freq == 0) //WTF??
+            continue;
+
+        for (int i = 1; i <= freq; i++) {
+            for (int j = 1; j <= amp; j++) {
+                for (int k = 0; k < j; k++)
+                    printf("%d", j);
+                printf("\n");
+            }
+            for (int j = amp - 1; j >= 1; j--) {
+                for (int k = 0; k < j; k++)
+                    printf("%d", j);
+                printf("\n");
+            }
+            if (i != freq)
+                printf("\n");
+        }
+
+        if (cases)
+            printf("\n");
+    }
+
+    return 0;
+}
